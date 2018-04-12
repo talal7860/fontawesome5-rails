eb84eb5e4211a50f0d1eba1797eefff05598f15f
Talal Arshad <talal7860@gmail.com>
2018-04-12 22:37:31 +0500

    adding fontaweomse5 files

---
+++ lib/fontawesome5/rails.rb
@@ -0,0 +1,8 @@
+require "fontawesome5/rails/version"
+
+module Fontawesome5
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ spec/fontawesome5/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Fontawesome5::Rails do
+  it 'has a version number' do
+    expect(Fontawesome5::Rails::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
